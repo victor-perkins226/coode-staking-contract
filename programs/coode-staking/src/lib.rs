433c498bf433178374053931fb69135a679563d0
victor-perkins226 <superdev0802@gmail.com>
2023-04-18 12:20:51 +0900

    update

--- programs/coode-staking/src/lib.rs
+++ programs/coode-staking/src/lib.rs
@@ -1,5 +1,6 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token::{self, TokenAccount, Token, Mint };
+use anchor_spl::token::{self, TokenAccount, Token, Mint,Transfer };
+use anchor_spl::associated_token::AssociatedToken;
 use anchor_lang::solana_program::{clock};
 use crate::constants::*;
 
@@ -7,10 +8,12 @@ declare_id!("A8FFovyJCj8J235sKfUiorYZB9o8qoLSG3EzGG554Ws");
 
 mod constants {
     use anchor_lang::prelude::Pubkey;
+    use anchor_lang::ToAccountInfo;
 
     pub const ADMIN_KEY: Pubkey = anchor_lang::solana_program::pubkey!("3ttYrBAp5D2sTG2gaBjg8EtrZecqBQSBuFRhsqHWPYxX"); 
     pub const COLLECTION_KEY: Pubkey = anchor_lang::solana_program::pubkey!("DyKv1WTgSyyPuHLi3SmFKMcoUDDif2KvRr55N8ZcU2oV");
-    
+    pub const TOKEN_KEY: Pubkey = anchor_lang::solana_program::pubkey!("55u5jMiJtwsvyo834R2mmcrxMGu7x2KvbrguJNbHFnEJ");
+
     pub const DECIMAL: u64 = 1000000000;
     pub const DAY_TIME: u32 = 86400;
     pub const STATISTIC_SEEDS: &str = "statistic";
@@ -32,6 +35,7 @@ pub mod degen_staking {
     pub fn initialize(ctx: Context<InitializeContext>) -> Result<()> {
         let a_statistic = &mut ctx.accounts.statistic;
         a_statistic.staked_count = 0;
+        a_statistic.token_count = 0;
 
         Ok(())
     }
@@ -129,6 +133,8 @@ pub mod degen_staking {
         let a_mint = &ctx.accounts.mint;
         let a_edition = &ctx.accounts.edition;
         let a_metadata_id = &ctx.accounts.metadata_id;
+        let a_ata_from = &mut ctx.accounts.ata_from;
+        let a_ata_to = &mut ctx.accounts.ata_to;
         let a_token_account = &ctx.accounts.token_account;
         let a_token_program = &ctx.accounts.token_program;
         let clock = clock::Clock::get().unwrap();
@@ -189,7 +195,7 @@ pub mod degen_staking {
                 end_time += DAY_TIME * DAYS[i] as u32;
                 if start_time <= end_time {
                     if end_time < current_time {
-                        if DAILY_REWARD - i >5 {
+                        if DAILY_REWARD - i as u32 >5 {
                             total_reward += (DAILY_REWARD - i as u32 ) as u64 * (end_time - start_time) as u64 * DECIMAL / DAY_TIME as u64 ;
                             start_time = end_time;
                             msg!("i: {}, total_reward: {}", i, total_reward);
@@ -200,7 +206,7 @@ pub mod degen_staking {
                         }
                     }
                     else {
-                        if DAILY_REWARD - i >5 {
+                        if DAILY_REWARD - i as u32 >5 {
                             msg!("i: {}, total_reward {}", i, total_reward);
                             total_reward += (DAILY_REWARD - i as u32 ) as u64 * (current_time - start_time)  as u64 * DECIMAL / DAY_TIME as u64;
                             break;
@@ -218,6 +224,16 @@ pub mod degen_staking {
 
         a_pool.total_reward = total_reward;
 
+         let cpi_token_accounts = Transfer {
+          from: a_ata_from.to_account_info().clone(),
+          to: a_ata_to.to_account_info().clone(),
+          authority: a_user.to_account_info().clone(),
+         };
+         let cpi_token_ctx = CpiContext::new(a_token_program.to_account_info().clone(), cpi_token_accounts);
+
+        token :: transfer(cpi_token_ctx, total_reward as u64 );
+        a_statistic.token_count -= total_reward;
+
         Ok(())
     }
 
@@ -226,6 +242,9 @@ pub mod degen_staking {
         let a_statistic = &mut ctx.accounts.statistic;
         let a_pool = &mut ctx.accounts.pool;
         let a_pool_data = &mut ctx.accounts.pool_data;
+        let a_ata_from = &mut ctx.accounts.ata_from;
+        let a_ata_to = &mut ctx.accounts.ata_to;
+        let a_token_program = &ctx.accounts.token_program;
         let clock = clock::Clock::get().unwrap();
         
         a_statistic.staked_count -= 1;
@@ -241,7 +260,7 @@ pub mod degen_staking {
                 end_time += DAY_TIME * DAYS[i] as u32;
                 if start_time <= end_time {
                     if end_time < current_time {
-                        if DAILY_REWARD - i >5 {
+                        if DAILY_REWARD - i as u32 >5 {
                             total_reward += (DAILY_REWARD - i as u32 ) as u64 * (end_time - start_time) as u64 * DECIMAL / DAY_TIME as u64 ;
                             start_time = end_time;
                             msg!("i: {}, total_reward: {}", i, total_reward);
@@ -252,7 +271,7 @@ pub mod degen_staking {
                         }
                     }
                     else {
-                        if DAILY_REWARD - i >5 {
+                        if DAILY_REWARD - i as u32 >5 {
                             msg!("i: {}, total_reward {}", i, total_reward);
                             total_reward += (DAILY_REWARD - i as u32 ) as u64 * (current_time - start_time)  as u64 * DECIMAL / DAY_TIME as u64;
                             break;
@@ -269,15 +288,46 @@ pub mod degen_staking {
         }
 
         a_pool.total_reward = total_reward;
+
+        let cpi_token_accounts = Transfer {
+          from: a_ata_from.to_account_info().clone(),
+          to: a_ata_to.to_account_info().clone(),
+          authority: a_user.to_account_info().clone(),
+         };
+         let cpi_token_ctx = CpiContext::new(a_token_program.to_account_info().clone(), cpi_token_accounts);
+
+        token :: transfer(cpi_token_ctx, total_reward as u64 );
+        a_statistic.token_count -= total_reward;
+
         Ok(())
 
+    }
+
+    pub fn token_transfer(ctx:Context<TokenTransferContext>, amount : u64) -> Result<()> {
+        let a_admin = &ctx.accounts.admin;
+        let a_statistic = &mut ctx.accounts.statistic;
+        let a_ata_from = &mut ctx.accounts.ata_from;
+        let a_ata_to = &mut ctx.accounts.ata_to;
+        let a_token_program = &ctx.accounts.token_program;
+        
+         let cpi_accounts = Transfer {
+          from: a_ata_from.to_account_info().clone(),
+          to: a_ata_to.to_account_info().clone(),
+          authority: a_admin.to_account_info().clone(),
+         };
+         let cpi_ctx = CpiContext::new(a_token_program.to_account_info().clone(), cpi_accounts);
+
+        token :: transfer(cpi_ctx, amount as u64 );
+        a_statistic.token_count += amount;
+
+        Ok(())
     }
 
 }
 
 #[derive(Accounts)]
 pub struct InitializeContext<'info> {
-    #[account(init, seeds = [STATISTIC_SEEDS.as_ref()], bump, payer = admin, space = 8 + 4)]
+    #[account(init, seeds = [STATISTIC_SEEDS.as_ref()], bump, payer = admin, space = 8 + 8 + 4)]
     pub statistic: Account<'info, Statistic>,
     #[account(mut, constraint = admin.key() == ADMIN_KEY)]
     pub admin: Signer<'info>,
@@ -318,6 +368,17 @@ pub struct UnstakeContext<'info> {
     #[account(mut)]
     pub user: Signer<'info>,
     pub mint: Account<'info, Mint>,
+     #[account(constraint = pay_mint.key() == TOKEN_KEY)]
+    pub pay_mint: Account<'info, Mint>,
+    #[account(mut, constraint = ata_from.mint == pay_mint.key() && ata_from.owner == statistic.key())]
+    pub ata_from: Account<'info, TokenAccount>,
+    #[account(
+    init_if_needed,
+    payer = user,
+    associated_token::mint = pay_mint,
+    associated_token::authority = user
+    )]
+    pub ata_to: Account<'info, TokenAccount>,
     #[account(mut)]
     pub token_account: Box<Account<'info, TokenAccount>>,
     /// CHECK: it's not dangerous
@@ -325,6 +386,10 @@ pub struct UnstakeContext<'info> {
     /// CHECK: it's not dangerous
     pub metadata_id: AccountInfo<'info>,
     pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+    pub rent: Sysvar<'info, Rent> 
+
 }
 
 #[derive(Accounts)]
@@ -336,15 +401,52 @@ pub struct ClaimContext<'info> {
     #[account(mut, constraint = pool_data.user == user.key() && pool_data.mint == mint.key())]
     pub pool_data: Account<'info, PoolData>,
     pub mint: Account<'info, Mint>,
+    #[account(constraint = pay_mint.key() == TOKEN_KEY)]
+    pub pay_mint: Account<'info, Mint>,
+    #[account(mut, constraint = ata_from.mint == pay_mint.key() && ata_from.owner == statistic.key())]
+    pub ata_from: Account<'info, TokenAccount>,
+    #[account(
+    init_if_needed,
+    payer = user,
+    associated_token::mint = pay_mint,
+    associated_token::authority = user
+    )]
+    pub ata_to: Account<'info, TokenAccount>,
     #[account(mut)]
     pub user: Signer<'info>,
     pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+    pub rent: Sysvar<'info, Rent> 
 }
 
+#[derive(Accounts)]
+pub struct TokenTransferContext<'info> {
+     #[account(mut)]
+    pub statistic: Account<'info, Statistic>,
+    #[account(mut, constraint = admin.key() == ADMIN_KEY)]
+    pub admin: Signer<'info>,
+    #[account(constraint = pay_mint.key() == TOKEN_KEY)]
+    pub pay_mint: Account<'info, Mint>,
+    #[account(mut, constraint = ata_from.mint == pay_mint.key() && ata_from.owner == admin.key())]
+    pub ata_from: Account<'info, TokenAccount>,
+    #[account(
+        init_if_needed,
+        payer = admin,
+        associated_token::mint = pay_mint,
+        associated_token::authority = statistic
+    )]
+    pub ata_to: Account<'info, TokenAccount>,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+    pub rent: Sysvar<'info, Rent> 
+}
 
 #[account]
 pub struct Statistic {
-    pub staked_count: u32
+    pub staked_count: u32,
+    pub token_count : u64,
 }
 
 #[account]
